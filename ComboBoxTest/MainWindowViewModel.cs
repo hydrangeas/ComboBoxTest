7744e2aebc92c4964b56fe2953f4d04513ee77bf
Akira Otaka <hydrangeas.project@gmail.com>
2019-09-02 21:23:20 +0900

    initial commit

---
+++ ComboBoxTest/MainWindowViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace ComboBoxTest
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel()
+        {
+            ComboBoxList = new ObservableCollection<string>
+            {
+                "",
+                "test",
+                "abc",
+                "def"
+            };
+        }
+
+        private ObservableCollection<string> comboBoxList { get; set; }
+        public ObservableCollection<string> ComboBoxList
+        {
+            get => comboBoxList;
+            set
+            {
+                if (value == comboBoxList)
+                    return;
+                comboBoxList = value;
+                RaisePropertyChanged(nameof(ComboBoxList));
+            }
+        }
+
+        private string textBoxValue;
+        public string TextBoxValue
+        {
+            get => textBoxValue;
+            set
+            {
+                if (value is string)
+                {
+                    var _textBoxValue = TextBoxValue;
+                    textBoxValue = value;
+                    RaisePropertyChanged(nameof(TextBoxValue));
+
+                    if (value == "test")
+                    {
+                        // 例えば "test" という文字は選択された場合、選択できなくする
+
+                        // 元のデータをバックアップ
+                        var _comboBoxList = ComboBoxList.ToList();
+
+                        // リストを更新することで強制的にComboBoxの更新を行う
+                        ComboBoxList.Clear();
+                        _comboBoxList.ForEach(x => ComboBoxList.Add(x));
+
+                        // 元のデータを戻す
+                        textBoxValue = _textBoxValue;
+                    }
+                    RaisePropertyChanged(nameof(TextBoxValue));
+                }
+            }
+        }
+    }
+}
---
+++ ComboBoxTest/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComboBoxTest
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged(string propertyName) =>
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
